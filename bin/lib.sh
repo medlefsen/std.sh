4d2bdefe55e3fa80902419c53ca554aa5ee37cc7
Matt Edlefsen <matt.edlefsen@gmail.com>
2010-08-31 14:00:40 -0400

    Fixed lib.sh
    Fixed bug in lib.sh that caused the outputted lines to not be evaluated
    when it's output was sent to "eval"

--- bin/lib.sh
+++ bin/lib.sh
@@ -5,8 +5,8 @@ then
     LIBSH_PATH="$(dirname "$0")/../lib"
 fi
 
+echo "export LIBSH_PATH='$LIBSH_PATH' ;"
+echo 'source "$LIBSH_PATH/bootstrap.sh" ;'
 echo '# Usage'
 echo '# Put "export LIBSH_PATH=path/to/lib.sh/lib" in your .bashrc or script'
 echo '# Put "eval `lib.sh`" at the top of your script'
-echo "export LIBSH_PATH='$LIBSH_PATH';"
-echo 'source "$LIBSH_PATH/bootstrap.sh"'
