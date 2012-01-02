be59f87548ff315da9d6fad5e1fb8ef51ae79160
Matt Edlefsen <matt@xforty.com>
2012-01-02 10:42:14 -0500

    Add eval? function to test if you're in an @eval

--- lib/eval.sh
+++ lib/eval.sh
@@ -2,15 +2,16 @@
 
 if [ -n "$LIBSH_DEBUG" ]
 then
-    _LIBSH_EVAL_PREFIX='eval "CMD='\''$($_LIBSH_EVAL_CMD)'\''; echo \"\$CMD\"; eval \$CMD #"'
+    _LIBSH_EVAL_PREFIX='eval "CMD='\''$(_LIBSH_IS_EVAL=1 $_LIBSH_EVAL_CMD)'\''; echo \"\$CMD\"; eval \$CMD #"'
 else
-    _LIBSH_EVAL_PREFIX='eval "$($_LIBSH_EVAL_CMD) #" '
+    _LIBSH_EVAL_PREFIX='eval "$(_LIBSH_IS_EVAL=1 $_LIBSH_EVAL_CMD) #" '
 fi
 
 _LIBSH_EVAL_PREFIX_LENGTH="${#_LIBSH_EVAL_PREFIX}"
 alias @eval="$_LIBSH_EVAL_PREFIX"
 
-filter_eval_command()
+
+_libsh_filter-eval-command()
 {
    if [ "${1:0:$_LIBSH_EVAL_PREFIX_LENGTH}" == "$_LIBSH_EVAL_PREFIX" ]
    then
@@ -18,10 +19,21 @@ filter_eval_command()
    fi
 }
 
-trap 'filter_eval_command "$BASH_COMMAND"' DEBUG
+trap '_libsh_filter-eval-command "$BASH_COMMAND"' DEBUG
+
+as-eval()
+{
+    _LIBSH_IN_EVAL=1 "$@"
+}
+
 
 evalify()
 {
     @eval args funcname
     alias "@$funcname=@eval $funcname"
+}
+
+eval? ()
+{
+    [[ -n "$_LIBSH_IN_EVAL" ]]
 }
